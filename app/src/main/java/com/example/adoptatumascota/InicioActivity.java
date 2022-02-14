1c14e67336e2da43024b39fc46372fbe9bbc4b36
JeniferMP <81910320+JeniferMP@users.noreply.github.com>
2022-02-13 22:27:52 -0500

    Inicio y login

--- app/src/main/java/com/example/adoptatumascota/InicioActivity.java
+++ app/src/main/java/com/example/adoptatumascota/InicioActivity.java
@@ -2,6 +2,7 @@ package com.example.adoptatumascota;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class InicioActivity extends AppCompatActivity {
@@ -10,5 +11,25 @@ public class InicioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        Thread t_inicio= new Thread(){
+            @Override
+            public void run() {
+                super.run();
+
+                try {
+                    //Validar configuraciones
+                    //validar si hay acceso a internet
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    //cargar la siguiente actividad
+                    Intent i_login = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(i_login);
+                    finish(); //elimina la actividad anterior (no se puede regresar a la interfaz anterior)
+                }
+            }
+        };
+        t_inicio.start();
     }
 }
