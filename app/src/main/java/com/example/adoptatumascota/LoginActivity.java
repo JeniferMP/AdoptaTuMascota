7f644144041eb5849c856837458865fce04f80a6
JeniferMP <81910320+JeniferMP@users.noreply.github.com>
2022-02-14 00:13:14 -0500

    login

--- app/src/main/java/com/example/adoptatumascota/LoginActivity.java
+++ app/src/main/java/com/example/adoptatumascota/LoginActivity.java
@@ -40,6 +40,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
 
     private void iniciar_sesion(String usuario, String contrasenia) {
+
     }
 
     private void registrar_usuario() {
