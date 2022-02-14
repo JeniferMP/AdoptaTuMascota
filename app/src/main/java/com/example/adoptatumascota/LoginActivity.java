24288b460aa84f5f62964a6e5712fa411d31fa13
JeniferMP <81910320+JeniferMP@users.noreply.github.com>
2022-02-13 23:49:12 -0500

    login lógica

--- app/src/main/java/com/example/adoptatumascota/LoginActivity.java
+++ app/src/main/java/com/example/adoptatumascota/LoginActivity.java
@@ -2,13 +2,50 @@ package com.example.adoptatumascota;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText txt_usuario, txt_contrasenia;
+    Button btn_ingresar;
+    TextView lbl_registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        txt_usuario= findViewById(R.id.login_txt_usuario);
+        txt_contrasenia= findViewById(R.id.login_txt_contrasenia);
+        btn_ingresar= findViewById(R.id.login_btn_ingresar);
+        lbl_registro= findViewById(R.id.login_lbl_registrate);
+
+        btn_ingresar.setOnClickListener(this);
+        lbl_registro.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.login_btn_ingresar:
+                iniciar_sesion(txt_usuario.getText().toString().trim(), txt_contrasenia.getText().toString().trim());
+                break;
+            case R.id.login_lbl_registrate:
+                registrar_usuario();
+                break;
+        }
+    }
+
+
+    private void iniciar_sesion(String usuario, String contrasenia) {
+    }
+
+    private void registrar_usuario() {
+        Intent i_registro= new Intent(this, RegistroActivity.class);
+        startActivity(i_registro);
+    }
+
+
 }
