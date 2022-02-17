50e3481789070f55f4c404b617fe7a1aa854f139
MCC <676teg@gmail.com>
2022-02-16 22:53:38 -0500

    registro de anuncio

---
+++ app/src/main/java/com/example/adoptatumascota/RegistroAnuncioActivity.java
@@ -0,0 +1,16 @@
+package com.example.adoptatumascota;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistroAnuncioActivity extends AppCompatActivity {
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_anuncio);
+    }
+}
