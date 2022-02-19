2a13cc9ea33f8f38a9d4336436c8672cb256c46b
Victor Terán Huamán <n00198603@upn.pe>
2022-02-19 11:36:39 -0500

    Nuevo Registro Diseño

--- app/src/main/java/com/example/adoptatumascota/LoginActivity.java
+++ app/src/main/java/com/example/adoptatumascota/LoginActivity.java
@@ -6,15 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     EditText txt_usuario, txt_contrasenia;
     Button btn_ingresar;
-    CheckBox chk_recordar;
     TextView lbl_registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +19,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
         txt_usuario= findViewById(R.id.login_txt_usuario);
         txt_contrasenia= findViewById(R.id.login_txt_contrasenia);
-        chk_recordar= findViewById(R.id.login_chk_recordar);
         btn_ingresar= findViewById(R.id.login_btn_ingresar);
         lbl_registro= findViewById(R.id.login_lbl_registrate);
 
@@ -44,15 +40,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
 
     private void iniciar_sesion(String usuario, String contrasenia) {
-        if (chk_recordar.isChecked())
-            Toast.makeText(this,"Recordó sesión",Toast.LENGTH_SHORT).show();
-        if (usuario.equals("Adoptante")&& contrasenia.equals("12345678")){
-            Intent i_principal= new Intent(this, PrincipalActivity.class);
-            startActivity(i_principal);
-            finish();
-        }else {
-            Toast.makeText(this,"Credenciales Incorrectas",Toast.LENGTH_LONG).show();
-        }
 
     }
 
